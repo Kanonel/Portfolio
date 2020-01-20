0a75bad2b99c8987b2f84886bf571e582bc71006
Kanonel <kanonel@yandex.ru>
2020-01-20 09:56:55 +0300

    reliase v1

---
+++ src/scripts/skills.js
@@ -0,0 +1,17 @@
+console.log('this is skills module');
+
+$('.slider__preview').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-miniature'
+});
+$('.slider-miniature').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider__preview',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+});
