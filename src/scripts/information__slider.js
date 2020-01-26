b81cb6506eadd8ddbe24864a72cd1ddc04bbfc77
Kanonel <kanonel@yandex.ru>
2020-01-26 22:05:37 +0300

    Адаптив под desktop

---
+++ src/scripts/information__slider.js
@@ -0,0 +1,8 @@
+/*var elem = document.querySelector('.information__list');
+var flkty = new Flickity( elem, {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true
+});*/
