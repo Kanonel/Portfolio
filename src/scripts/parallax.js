be9d1bbdb52ebe8e7b16d42a22a27efa95ce8453
Kanonel <kanonel@yandex.ru>
2020-01-29 19:58:38 +0300

    Добавлен параллакс Welcome-секции

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,20 +1,16 @@
-var parallax = (function() {
-    var mountains = document.querySelector('.paralax-welcome__element--mountains');
-    var train = document.querySelector('.paralax-welcome__element--train');
-    var grass = document.querySelector('.paralax-welcome__element--grass');
-  
-    return {
-      move: function (block, windowScroll, strafeAmount) {
-        var strafe = (windowScroll) / strafeAmount + '%';
-        var transformString = 'translate3d(0,' + strafe + ', 0)';
-        var style = block.style;
-        style.transform = transformString;
-        style.webkitTransform = transformString;
-      }, 
-      init: function (wScroll) {
-        this.move(mountains, wScroll, -85);
-        this.move(train, wScroll, -70);
-        this.move(grass, wScroll, -55);
-      }
-    }
-}());
+const parallax = document.querySelector('.parallax-welcome');
+const element = parallax.children;
+
+function moveElementDependsOnScroll(wScroll) {
+  Array.from(element).forEach(layer => {
+    const divider = layer.dataset.speed;
+    const strafe = wScroll * divider / 10;
+
+    layer.style.transform = `translateY(-${strafe}%)`;
+  })
+}
+
+window.addEventListener('scroll', e => {
+  const wScroll = window.pageYOffset;
+  moveElementDependsOnScroll(wScroll);
+})
