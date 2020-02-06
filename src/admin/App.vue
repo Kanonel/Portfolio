8ff3cf006f076caa8e9234b612851d134ec347e2
Kanonel <kanonel@yandex.ru>
2020-02-06 21:52:00 +0300

    Исправила возникающую ошибку и добавила стили для header и about

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,10 +1,295 @@
+<style lang="postcss">
+  body {
+    font-family: "Open Sans", Helvetica, sans-serif;
+    font-size: 16px;
+    line-height: 1.42;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: $text-color;
+    margin: 0;
+  }
+
+  .wrapper {
+    overflow: hidden;
+    min-height: 100%;
+    position: relative;
+  }
+
+  .container {
+    margin: 0 auto;
+    max-width: 1800px;
+    width: 95%;
+  }
+
+  button {
+    border: 0;
+    cursor: pointer;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: normal;
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
+  button[type="button"] {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    cursor: pointer;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .header {
+    position: relative;
+    left: 0;
+    right: 0;
+    padding: 16px 0;
+    background-color: #3e3e59;
+    color: #ffffff;
+    font-family: "Open Sans", sans-serif;
+  }
+
+  .header__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header__user {
+    display: flex;
+    align-items: center;
+    width: 17%;
+    justify-content: space-between;
+  }
+
+  .header__avatar {
+    width: 45px;
+    height: 45px;
+  }
+
+  .header__img {
+    border-radius: 50%;
+  }
+  
+  .header__username {
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 34px;
+  }
+
+  .header__panel {
+    display: flex;
+    opacity: 0.5;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+  }
+
+  .header__purpose,
+  .header__exite {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 34px;
+  } 
+
+  .navigation {
+    box-shadow: 0 0 40px rgba(0, 0, 0, 0.2);
+  }
+
+  .nav__list {
+    display: flex;
+  }
+
+  .nav__item {
+    padding: 34px 30px;
+
+    &--active {
+      border-bottom: 3px solid #383bcf;
+    }
+  }
+
+  .nav__link {
+    color: #414c63;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 36.14px;
+    text-decoration: none;
+
+    &--active {
+      color: #4f52d4;
+    }
+  }
+
+  .about-me {
+    padding-top: 59px;
+  }
+
+  .about-me__header {
+    display: flex;
+    margin-bottom: 61px;
+  }
+
+  .about-me__title {
+    color: #414c63;
+    font-size: 21px;
+    font-weight: 700;
+    line-height: 34px;
+    margin-right: 65px;
+  }
+
+  .about-me__button {
+    position: relative;
+    background-color: #ffffff;
+    color: #383bcf;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 33.89px;
+    outline: none;
+
+    &:before {
+      content: "+";
+      position: absolute;
+      width: 21px;
+      height: 21px;
+      border-radius: 50%;
+      color: #ffffff;
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 15.19px;
+      background-image: linear-gradient(to right, #006aed 0%, #3f35cb 100%);
+      top: 50%;
+      right: 100%;
+      transform: translateY(-50%);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  .form-new {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .form-new__row {
+    width: 49%;
+    min-height: 415px;
+    padding: 31px;
+    box-shadow: 4px 3px 20px rgba(0, 0, 0, 0.07);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .form-new__header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .form-new__input--group {
+    width: 60%;
+    padding: 14px 0;
+    outline: none;
+    border: 0;
+    border-bottom: 3px solid #000000;
+    opacity: 0.51;
+    color: #414c63;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 34px;
+  }
+
+  .form-new__btn {
+    display: flex;
+    align-items: center;
+  }
+
+  .form-new__button--tick,
+  .form-new__button--cross {
+    width: 30px;
+    height: 30px;
+    background-color: #ffffff;
+  }
+
+  .form-new__addition {
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .form-new__input--skill {
+    width: 70%;
+    border: 0;
+    border-bottom: 3px solid #000000;
+    opacity: 0.5;
+    color: #373e42;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 34px;
+    padding: 0 20px;
+  }
+
+  .form-new__input--persent {
+    width: 15%;
+    color: #414c63;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 34px;
+    border: 0;
+    border-bottom: 3px solid #000000;
+    padding: 0 20px;
+  }
+
+  .form-new__button--plus {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: #ffffff;
+    font-size: 30px;
+    font-weight: 400;
+    line-height: 15.19px;
+    background-image: linear-gradient(to right, #006aed 0%, #3f35cb 100%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
+
 <template lang="pug">
   .wrapper
     header.header
       .container.header__container
         .header__user
           .header__avatar
-            +image('content/user.jpg', 'header__img', 'Аватар создателя сайта')
+            img(src="../images/content/user.jpg" class="header__img" alt="Аватар создателя сайта")
           .header__username Екатерина Корнилова
         .header__panel
           .header__purpose Панель администрирования
@@ -31,10 +316,11 @@
                 .form-new__row
                   .form-new__header
                     input.form-new__input.form-new__input--group(type="text" placeholder = "Название новой группы" required)
-                    button(type="button").form-new__button.form-new__button--tick
-                      +icon('tick', 'form-new__icon')
-                    button(type="button").form-new__button.form-new__button--cross
-                      +icon('cross', 'form-new__icon')
+                    .form-new__btn
+                      button(type="button").form-new__button.form-new__button--tick
+                        img(src="../images/content/admin/tick.png" class="form-new__icon" alt="Подтвердить")
+                      button(type="button").form-new__button.form-new__button--cross
+                        img(src="../images/content/admin/cross.png" class="form-new__icon" alt="Отменить")
                   .form-new__addition
                     input.form-new__input.form-new__input--skill(type="text" placeholder = "Новый навык" required)
                     input.form-new__input.form-new__input--persent(type="number" min="0" max="100" placeholder="0")
@@ -42,24 +328,25 @@
                 .form-new__row
                   .form-new__header
                     input.form-new__input.form-new__input--group(type="text" placeholder = "Название новой группы" required value="Frontend")
-                    button(type="button").form-new__button.form-new__button--tick
-                      +icon('pencil', 'form-new__icon')
+                    button(type="button").form-new__button.form-new__button--pencil
+                      img(src="../images/content/admin/pencil.png" class="form-new__icon" alt="Подтвердить")
                   .form-new__desk
-                    - var skills = [
-                      ['HTML', '30', '%',],
-                      ['CSS', '50', '%',],
-                      ['Javascript', '100', '%',]
-                    ]
-                      table.form-new__table
-                        each row in skills
-                          tr.form-new__rows
-                            each cell in row
-                              td.form-new__col #{cell}
-                            td.form-new__col
-                              button(type="button").form-new__button--pencil
-                                +icon('pencil', 'form-new__icon')
-                              button(type="button").about__button--trash
-                                +icon('trash', 'form-new__icon')
+                    - 
+                      var skills = [
+                        ['HTML', '30', '%',],
+                        ['CSS', '50', '%',],
+                        ['Javascript', '100', '%',]
+                      ]
+                    table.form-new__table
+                      each row in skills
+                        tr.form-new__rows
+                          each cell in row
+                            td.form-new__col #{cell}
+                          td.form-new__col
+                            button(type="button").form-new__button--pencil
+                              img(src="../images/content/admin/pencil.png" class="form-new__icon" alt="Изменить")
+                            button(type="button").about__button--trash
+                              img(src="../images/content/admin/trash.png" class="form-new__icon" alt="Удалить")
                   .form-new__addition
                     input.form-new__input.form-new__input--skill(type="text" placeholder = "Новый навык" required)
                     input.form-new__input.form-new__input--persent(type="number" min="0" max="100" placeholder="0")
@@ -113,7 +400,7 @@
           .rewiews__new
             .rewiews__user
               .rewiews__avatar
-                +icon('user', 'rewiews__icon')
+                img(src="../images/content/admin/user.png" class="form-new__icon" alt="Аватар")
                 .rewiews__text Добавить фото
             .rewiews__information
               .rewiews__row
