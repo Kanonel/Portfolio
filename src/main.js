9b0596295bc0ef3af2d7028c912324bb1277184a
Kanonel <kanonel@yandex.ru>
2020-02-20 22:43:59 +0300

    Исправлены замечания наставника

--- src/main.js
+++ src/main.js
@@ -6,9 +6,8 @@ if (process.env.NODE_ENV === "development") {
 import "./scripts/skills";
 import "./scripts/information__slider";
 import "./scripts/hamburger-menu";
-import "./scripts/parallax";
+import "./scripts/parallax-welcome";
 import "./scripts/parallax-general";
-import "./scripts/parallax-off-scroll";
 import "./scripts/myworks-slider";
 import sendForm from './scripts/sendForm.js';
 
